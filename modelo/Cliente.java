ecd595e7e5480bab0fece44ea7d74e74bbb69a88
S_Franco <101674377+KaizoTrap@users.noreply.github.com>
2024-10-28 07:01:07 -0500

    Create Cliente.java

---
+++ modelo/Cliente.java
@@ -0,0 +1,36 @@
+package modelo;
+
+
+public class Cliente {
+    
+    private int id_cliente;
+    private String usuario;
+    private String password;
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
