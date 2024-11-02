a2c1bab91efd310751646f5b1ff033330d9db93a
S_Franco <101674377+KaizoTrap@users.noreply.github.com>
2024-11-02 17:54:22 -0500

    Update Factura.java

--- modelo/Factura.java
+++ modelo/Factura.java
@@ -3,6 +3,30 @@ package modelo;
 public class Factura {
     private DetallePedido detallepedido;
     private DetalleCliente detallecliente;
-    
-    
+    private String fechapedido;
+
+    public DetallePedido getDetallepedido() {
+        return detallepedido;
+    }
+
+    public void setDetallepedido(DetallePedido detallepedido) {
+        this.detallepedido = detallepedido;
+    }
+
+    public DetalleCliente getDetallecliente() {
+        return detallecliente;
+    }
+
+    public void setDetallecliente(DetalleCliente detallecliente) {
+        this.detallecliente = detallecliente;
+    }
+
+    public String getFechapedido() {
+        return fechapedido;
+    }
+
+    public void setFechapedido(String fechapedido) {
+        this.fechapedido = fechapedido;
+    }
+      
 }
