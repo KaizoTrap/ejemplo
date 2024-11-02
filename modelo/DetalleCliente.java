64a67f11312a549569c72f6f510e4ea0c63aa6d3
S_Franco <101674377+KaizoTrap@users.noreply.github.com>
2024-11-02 17:53:41 -0500

    Update and rename DetalleCliente to DetalleCliente.java

---
+++ modelo/DetalleCliente.java
@@ -0,0 +1,55 @@
+package modelo;
+
+
+public class DetalleCliente {
+    
+    private int id_dato;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private int identificacion;
+
+    public int getId_dato() {
+        return id_dato;
+    }
+
+    public void setId_dato(int id_dato) {
+        this.id_dato = id_dato;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+    
+    
+    
+    
+}
